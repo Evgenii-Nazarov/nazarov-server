c050550ca91cd65da0ea2e4d599fc95de3d22f92
Evgenii-Nazarov <ev66647260@gmail.com>
2020-08-20 16:12:32 -0700

    changed env vars for mongo connection

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -3,9 +3,7 @@ import mongoose from 'mongoose';
 export default function mongoConnection() {
   mongoose.set('useCreateIndex', true);
   mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${
-      process.env.MONGO_HOST
-    }/${process.env.MONGO_DB_NAME}`,
+    `mongodb+srv://admin:fTz4WtCuCdzMMwLV@cluster0-za2nd.mongodb.net/todo`,
     { useNewUrlParser: true },
   );
 
