64112f8abf10faaade1657b03d6225d1bf743fa3
Evgenii Nazarov <evgeniinazarov@Evgeniis-MacBook-Pro.local>
2023-10-27 15:58:31 -0700

    update mongo connection

--- src/modules/core/db.js
+++ src/modules/core/db.js
@@ -3,14 +3,12 @@ import mongoose from 'mongoose';
 export default function mongoConnection() {
   mongoose.set('useCreateIndex', true);
   const res = mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${
-      process.env.MONGO_HOST
-    }/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true },
   );
 
-  mongoose.connection.on('error', (err) => {
-    console.info('error', err)
+  mongoose.connection.on('error', err => {
+    console.info('error', err);
     throw new Error('Unable to connect to database');
   });
 
